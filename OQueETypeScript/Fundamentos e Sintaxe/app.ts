8b2acfd24137b1337988f8f240b2b3db0f9ddd45
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-02-15 16:52:47 -0300

    Objetos e Interfaces

--- OQueETypeScript/Fundamentos e Sintaxe/app.ts
+++ OQueETypeScript/Fundamentos e Sintaxe/app.ts
@@ -8,3 +8,27 @@ m = 'Ben'
 m = false
 
 //Objetos e Interfaces
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string //caso esteja com o seguinte sinal ?: siginifica que é opcional
+}
+const pessoa: Pessoa = {
+    nome: "Ben",
+    idade: 34
+}
+
+const outraPessoa: Pessoa = {
+    nome: "Hatus",
+    idade: 30,
+    profissao: "Dev Full-stack"
+}
+
+const arrayPessoa: Array<Pessoa> = [
+    pessoa,
+    outraPessoa
+]
+
+const arrayNum: number[] = [1, 2, 3]
+
+const arrayString: Array<string> = [ "1", "2", "3" ]
