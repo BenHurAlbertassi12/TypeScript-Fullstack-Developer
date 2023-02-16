0416ad2af4bdc7bbf876962bfc4693dd04d63e39
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-02-16 01:18:31 -0300

    ReadOnly

--- Fundamentos do TypeScript/Fundamentos e Sintaxe 2/app.ts
+++ Fundamentos do TypeScript/Fundamentos e Sintaxe 2/app.ts
@@ -1,11 +1,16 @@
+import { CompanyAccount } from './class/CompanyAccount';
 import { PeopleAccount } from './class/PeopleAccount';
 
 const benAccount: PeopleAccount = new PeopleAccount(1, 'Ben', 34);
 console.log(benAccount);
 benAccount.setname('BenHur Albertassi');
+benAccount.deposit();
 console.log(benAccount);
 console.log(benAccount.getName());
 
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 1);
+companyAccount.deposit();
+// companyAccount.accountNumber = 11 não é possivel
 
 
 
--- Fundamentos do TypeScript/Fundamentos e Sintaxe 2/class/DioAccount.ts
+++ Fundamentos do TypeScript/Fundamentos e Sintaxe 2/class/DioAccount.ts
@@ -1,8 +1,8 @@
 export abstract class DioAccount {
   private name: string;
-  accountNumber: number;
+  private readonly accountNumber: number;
   balance: number = 0;
-  status: boolean = true
+  private status: boolean = true;
 
   constructor(name: string, accountNumber: number) {
     this.name = name;
@@ -10,14 +10,14 @@ export abstract class DioAccount {
   }
 
   setname = (name: string): void => {
-    this.name = name
+    this.name = name;
     console.log('Nome alterado com sucesso!');
-  }
-  getName = ():string => {
-    return this.name
-  }
+  };
+  getName = (): string => {
+    return this.name;
+  };
   deposit = (): void => {
-    if(this.validateStatus()) console.log('Voce depositou');
+    if (this.validateStatus()) console.log('Voce depositou');
   };
   whithdraw = (): void => {
     console.log('Voce retirou');
@@ -26,7 +26,7 @@ export abstract class DioAccount {
     console.log(this.balance);
   };
   private validateStatus = (): boolean => {
-    if(this.status) this.status
-    throw new Error() 
-  }
+    if (this.status) this.status;
+    throw new Error();
+  };
 }
