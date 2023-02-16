9960f928e8f1dd2346962399ccab6982a534ab26
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-02-15 22:48:44 -0300

    Fundamentos do TypeScript

---
+++ Fundamentos do TypeScript/Fundamentos e Sintaxe 1/app.ts
@@ -0,0 +1,95 @@
+// Variaveis e Tipos
+let b: string = 'b'
+let n: number = 2
+let x: boolean = true
+
+let m: any = 2
+m = 'Ben'
+m = false
+
+//Objetos e Interfaces
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string //caso esteja com o seguinte sinal ?: siginifica que é opcional
+}
+const pessoa: Pessoa = {
+    nome: "Ben",
+    idade: 34
+}
+
+const outraPessoa: Pessoa = {
+    nome: "Hatus",
+    idade: 30,
+    profissao: "Dev Full-stack"
+}
+
+const arrayPessoa: Array<Pessoa> = [
+    pessoa,
+    outraPessoa
+]
+
+const arrayNum: number[] = [1, 2, 3]
+
+const arrayString: Array<string> = ["1", "2", "3"]
+
+//Decisões e Repetições
+
+const num: number = 15
+if (num > 15) {
+    console.log('Num maior que 15');    
+} else if(num === 15) {
+    console.log('Num igual a 15');    
+} else {
+    console.log('Num menor que 15');    
+}
+
+const typeUser = {
+    adm: 'Seja bem vindo admin',
+    student: 'Voce é um estudante',
+    viwer: 'Voce pode visualizar',
+}
+function validateUser(user: string) {
+    console.log(typeUser[user as keyof typeof typeUser]);  
+}
+const usuario = 'adm'
+validateUser(usuario)
+validateUser('student')
+validateUser('viwer')
+
+// Estrutura de Repetição
+//for
+for (let index = 0; index < 5; index++) {
+    console.log(index);
+}
+///while
+let numW = 2
+while (numW < 6) {
+    console.log(n);
+    n++
+}
+
+//Array
+const arrayNumb: Array<number> = [1,2,3,4,5]
+const arrayStri: string[] = ["1", "2", "3", "4", "5"]
+console.log(arrayNumb[0]);
+console.log(arrayNumb.length);
+console.log(arrayNumb, arrayNumb.length);
+arrayNum.push(6)
+console.log(arrayNumb, arrayNumb.length);
+arrayNum.pop()
+console.log(arrayNumb, arrayNumb.length);
+
+// Manipulando Arrays
+const buscaNum = arrayNumb.find(num => num === 4)
+console.log(buscaNum);
+
+arrayNumb.forEach(num => console.log(num))
+arrayNumb.forEach(num => {
+    if(num > 2 && num % 2 === 0)
+    console.log(num)
+})
+
+arrayNum.map(num => console.log(num))
+
+
