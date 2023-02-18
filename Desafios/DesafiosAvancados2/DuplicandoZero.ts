5679244ebfe515d8d3578f829a14818f1edec09e
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-02-18 15:11:29 -0300

    Reoganizando

--- Desafios/DesafiosAvancados2/DuplicandoZero.ts
+++ Desafios/DesafiosAvancados2/DuplicandoZero.ts
@@ -1,19 +1,16 @@
-let N = parseInt(gets()); 
+let N = parseInt(gets());
 
-let arr = new Array(N); 
+let arr = new Array(N);
 
-for (let i = 0; i < N; i++){ 
+for (let i = 0; i < N; i++) {
+  arr[i] = parseInt(gets());
 
-    arr[i] = parseInt(gets());
-    
-    if(arr[i] === 0){
-      if((i+1) < N){
-        arr[i+1] = 0;
-        i++;  
-      }  
-      
+  if (arr[i] === 0) {
+    if (i + 1 < N) {
+      arr[i + 1] = 0;
+      i++;
     }
+  }
+}
 
-}    
- 
 print(arr);
